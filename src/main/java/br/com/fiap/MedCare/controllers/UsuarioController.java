d572bffc334ae35297c612b80a2d1d2c400c1e04
tiemyz <rm95145@fiap.com.br>
2023-11-22 11:53:59 -0300

    crud e paginação implementados no usuário

--- src/main/java/br/com/fiap/MedCare/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/MedCare/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,8 +13,14 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.MedCare.models.Credencial;
@@ -22,6 +30,7 @@ import br.com.fiap.MedCare.service.TokenService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     @Autowired
@@ -36,7 +45,7 @@ public class UsuarioController {
     @Autowired
     TokenService tokenService;
 
-    @PostMapping("/api/registrar")
+    @PostMapping("/registrar")
     public ResponseEntity<Object> registrar(@RequestBody @Valid Usuario usuario){
         String tipoUsuario = getTipoUsuario(usuario.getEmail());
         usuario.setTipoUsuario(tipoUsuario);
@@ -51,26 +60,68 @@ public class UsuarioController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/api/login")
-    public ResponseEntity<Object> login(@RequestBody Credencial credencial){
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody Credencial credencial) {
         try {
             Authentication auth = manager.authenticate(credencial.toAuthentication());
             UserDetails userDetails = (UserDetails) auth.getPrincipal();
-
+    
             String tipoUsuario = getTipoUsuario(userDetails.getUsername());
-
             var token = tokenService.generateToken(credencial);
-            
+    
             Map<String, Object> response = new HashMap<>();
             response.put("tipoUsuario", tipoUsuario);
             response.put("token", token);
-
+            response.put("nome", userDetails.getUsername()); // Adicione o nome do usuário
+    
             return ResponseEntity.ok(response);
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar a solicitação");
         }
+    }
+    
+
+    @GetMapping
+    public ResponseEntity<Page<Usuario>> getAllUsuarios(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<Usuario> usuarios = repository.findAll(PageRequest.of(page, size));
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateUsuario(@PathVariable Long id, @RequestBody @Valid Usuario updatedUsuario) {
+        return repository.findById(id)
+                .map(existingUsuario -> {
+                    // Update fields of existingUsuario with the values from updatedUsuario
+                    existingUsuario.setNome(updatedUsuario.getNome());
+                    existingUsuario.setEmail(updatedUsuario.getEmail());
+                    // ... (update other fields)
+
+                    repository.save(existingUsuario);
+
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUsuario(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(usuario -> {
+                    repository.delete(usuario);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
     private String getTipoUsuario(String email) {
--- src/main/java/br/com/fiap/MedCare/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/MedCare/repository/UsuarioRepository.java
@@ -1,13 +1,18 @@
 package br.com.fiap.MedCare.repository;
 
-import java.util.Optional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.MedCare.models.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+import java.util.Optional;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
-    
+
+    // You can add custom query methods for specific filtering if needed
+
+    // Example of a custom query method for finding users by type
+    Page<Usuario> findByTipoUsuario(String tipoUsuario, Pageable pageable);
 }
