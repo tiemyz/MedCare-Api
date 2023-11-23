3277a5f013bc310ea2f25ee60e700518d2f43c72
tiemyz <rm95145@fiap.com.br>
2023-11-23 11:36:32 -0300

    att

--- src/main/java/br/com/fiap/MedCare/config/WebConfig.java
+++ src/main/java/br/com/fiap/MedCare/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:19006")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }    
