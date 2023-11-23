9d58bfd2d40bc04061d4b57a3bd90981049f7c6c
tiemyz <rm95145@fiap.com.br>
2023-11-23 16:23:16 -0300

    att

--- src/main/java/br/com/fiap/MedCare/models/Mensagem.java
+++ src/main/java/br/com/fiap/MedCare/models/Mensagem.java
@@ -6,8 +6,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Mensagem {
 
--- src/main/java/br/com/fiap/MedCare/models/Usuario.java
+++ src/main/java/br/com/fiap/MedCare/models/Usuario.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @AllArgsConstructor
